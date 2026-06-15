e61a9ef2edc5dd738e593153878d4b67e32be749
Michel Hardenberg <au617623@uni.au.dk>
2026-06-15 08:34:52 +0200

    up

--- build/CMakeFiles/slides_pdf.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/slides_pdf"
-  "slides.pdf"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/slides_pdf.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/talk_pdf.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/talk_pdf"
-  "talk.pdf"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/talk_pdf.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
